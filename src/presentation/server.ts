9b38444a3b3c71fd883e2129aa7be1633993075a
saleel vt <saleelvt57@gmail.com>
2024-10-28 00:29:25 +0530

    Add : Socket.io integrated

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,25 +1,42 @@
 import express, { Application, Request, Response } from "express";
+
 import dotenv from "dotenv";
 import { adminRoutes } from "./../infrastructure/routers";
 import { routes, salonRoutes } from "@/infrastructure/routers";
 import cookieParser from "cookie-parser";
-
+import { Server } from "socket.io";
+import http from "http"; 
 import cors from "cors";
 import { dependencies } from "@/boot/dependencies";
 import { adminDependencies } from "@/boot/adminDependencies";
 import { salonDependencies } from "@/boot/salonDependencies";
 dotenv.config();
  
+const allowedOrigin = process.env.CLIENT_URL;
+const clientId = process.env.CLIENT_URL;
+console.log(`Client URL: ${clientId}`);
 const app: Application = express();
+
 const PROT: number = Number(process.env.PORT) || 4001;
+
+
+const server= http.createServer(app)
+
+
+const io= new Server(server,{
+  cors:{
+    origin: process.env.CLIENT_URL, // Allow frontend origin
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+  
+  pingTimeout: 60000,
+  transports: ['websocket', 'polling']
+})
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-const allowedOrigin = process.env.CLIENT_URL;
-const clientId = process.env.CLIENT_URL;
-console.log(`Client URL: ${clientId}`);
 
 const corsOptions = {
   origin: allowedOrigin,
@@ -33,12 +50,38 @@ app.use("/", routes(dependencies));
 app.use("/salon", salonRoutes(salonDependencies));
 app.use("/admin", adminRoutes(adminDependencies));
 
+
+
+io.on("connection",(socket)=>{
+
+  console.log("new Client Connected ", socket.id);
+  
+  socket.on("send_message",(messageData)=>{
+    console.log("Message received on server:", messageData);
+
+     // Broadcast message to all clients
+   io.emit("receive_message", messageData);
+
+  })
+
+
+   // Handle client disconnection
+   socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+  
+
+})
+
+
+
+
 app.use("*", (req: Request, res: Response) => {
   res
     .status(404)
     .json({ success: false, status: 404, message: "Api Not Found " });
 });
-app.listen(PROT, () => {
+server.listen(PROT, () => {
   console.log(`mongodb connected successfully on this ${PROT}`);
 });
 
